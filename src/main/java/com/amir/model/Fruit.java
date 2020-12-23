190d424af401a46608665061e66c1749ae9e4ebf
amirelrahep <amirelrahep@gmail.com>
2020-12-22 21:45:35 -0500

    Updated Fruit.java class comments

--- src/main/java/com/amir/model/Fruit.java
+++ src/main/java/com/amir/model/Fruit.java
@@ -17,10 +17,15 @@ public class Fruit {
 
     private Rectangle fruit;
     private final Color fruitColor;
-
     private static final Random rand = new Random();
 
 
+    /**
+     * This is the Fruit constructor. Creates a Fruit object this is represented by a Rectangle object.
+     * Takes in a Color representing the Fruit color.
+     *
+     * @param color color of the fruit
+     */
     public Fruit(Color color) {
         this.fruit = new Rectangle();
         this.fruitColor = color;
@@ -29,11 +34,20 @@ public class Fruit {
     }
 
 
+    /**
+     * This method draws the Fruit. It does this by adding the Fruit to the pane.
+     *
+     * @param pane pane to draw the fruit on
+     */
     public void drawFruit(Pane pane) {
         pane.getChildren().add(this.fruit);
     }
 
 
+    /**
+     * This method moves the Fruit. Generates two numbers within the boundaries of the pane. Sets the LayoutX and
+     * LayoutY of the Fruit to the new location.
+     */
     public void moveFruit() {
         int xPos = -1;
         int yPos = -1;
@@ -50,12 +64,20 @@ public class Fruit {
 
     // private methods
 
+    /**
+     * This method creates the Fruit object. Sets the Fruit to a new Rectangle and calls the moveFruit method.
+     */
     private void createFruit() {
         this.fruit = new Rectangle(20, 20, fruitColor);
         moveFruit();
     }
 
 
+    /**
+     * This method return the Fruit object.
+     *
+     * @return fruit
+     */
     public Rectangle getFruit() {
         return fruit;
     }
